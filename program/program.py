5a05df96b1c17630e4058cf3a6fe4ffa42392b33
Szymon <bilinskiszymon123@gmail.com>
2023-01-05 17:55:02 +0100

    init

---
+++ program/program.py
@@ -0,0 +1,15 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+ 
+def Window():
+    app=QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(200,200,900,500)
+    win.window
+    win.show()
+    sys.exit(app.exec_())
+
+ 
+
+window = Window()
+    
