166d454ed66157253b25a9c64f5d2a2dd395f15f
kamilstpn <kamil.stepien.st@gmail.com>
2023-01-06 12:26:56 +0100

    tekst2

--- program/program.py
+++ program/program.py
@@ -1,6 +1,6 @@
 
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow,QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow,QWidget, QLabel, QLineEdit, QHBoxLayout
 from PyQt5.QtGui import *
 
 import sys
@@ -12,7 +12,76 @@ class GetDataFromUserWindow(QWidget):
         self.setWindowTitle("Enter Data")
         
 
+class EnterDataWindow(QMainWindow):
+    def __init__(self):
+        super(EnterDataWindow,self).__init__()
+        self.setGeometry(200,200,900,500)
+        self.setWindowTitle("Wprowadzanie danych")
+        self.initData()
 
+    def initData(self):
+    
+        self.MainTitleLabel = QtWidgets.QLabel(self)
+        self.MainTitleLabel.setText("Wprowadz dane")
+        self.MainTitleLabel.setFont(QFont('Arial', 16))
+        self.MainTitleLabel.move(350,20)
+        self.MainTitleLabel.adjustSize()
+
+        # ukladH4=QHBoxLayout
+        # self.labelR = QLabel('R')
+        # self.labelG = QLabel('G')
+        # self.labelB = QLabel('B')
+        # self.kolorR = QLineEdit('0')
+        # self.kolorG = QLineEdit('0')
+        # self.kolorB = QLineEdit('0')
+        # for v in ('R', 'G', 'B'):
+        #     label = getattr(self, 'label' + v)
+        #     kolor = getattr(self, 'kolor' + v)
+        #     ukladH4.addWidget(label)
+        #     ukladH4.addWidget(kolor)
+        #     kolor.setMaxLength(3)
+    
+
+        self.Capacity = QtWidgets.QLabel(self)
+        self.Capacity.setText("Pojemność pojazdów")
+        self.Capacity.setFont(QFont('Arial', 13))
+        self.Capacity.move(250,100)
+        self.Capacity.adjustSize()
+        
+        self.Availability = QtWidgets.QLabel(self)
+        self.Availability.setText("Dostępna liczba pojazdów")
+        self.Availability.setFont(QFont('Arial', 13))
+        self.Availability.move(250,200)
+        self.Availability.adjustSize()
+ 
+        self.Time = QtWidgets.QLabel(self)
+        self.Time.setText("Czas podróży w ciągu dnia")
+        self.Time.setFont(QFont('Arial', 13))
+        self.Time.move(250,300)
+        self.Time.adjustSize()
+
+        self.continue_enterData_button = QtWidgets.QPushButton(self)
+        self.continue_enterData_button.setText("Kontynuuj")
+        self.continue_enterData_button.move(800,0)
+        self.continue_enterData_button.clicked.connect(self.continue_enterData_button_clicked)
+
+        self.go_back_button = QtWidgets.QPushButton(self)
+        self.go_back_button.setText("Wstecz")
+        self.go_back_button.move(0,0)
+        self.go_back_button.clicked.connect(self.go_back_button_clicked)
+
+        self.add_client_button = QtWidgets.QPushButton(self)
+        self.add_client_button.setText("Dodaj klienta")
+        self.add_client_button.setFont(QFont('Arial', 16))
+        self.add_client_button.setGeometry(330,390,230,100)
+        #self.add_client_button.move(370,400)
+        self.add_client_button.clicked.connect(self.add_client_button_clicked)
+    def continue_enterData_button_clicked(self):
+        self.go_back_button.setEnabled(False)
+    def go_back_button_clicked(self):
+        self.continue_enterData_button.setEnabled(False)
+    def add_client_button_clicked(self):
+        self.add_client_button.setEnabled(False)
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow,self).__init__()
@@ -56,8 +125,10 @@ class MainWindow(QMainWindow):
         self.start_simulation_button.setEnabled(False)
 
     def get_data_from_user_button_clicked(self):
-        self.w1=GetDataFromUserWindow()
+        self.w1=EnterDataWindow()
         self.w1.show()
+        #self.w1=GetDataFromUserWindow()
+        #self.w1.show()
         #self.get_data_from_user.setEnabled(False)
 
     def read_data_from_csv_button_clicked(self):
